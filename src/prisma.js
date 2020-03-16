a2611a8ec72a4431a0ddbc5ae926b2c1715c9baf
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-16 17:13:44 +0100

    feat(src/prisma.js) Challenge get comments
    Get comments code for the challenge

--- src/prisma.js
+++ src/prisma.js
@@ -8,3 +8,11 @@ const prisma = new Prisma({
 prisma.query.users(null, '{ id name email posts { id title } }').then(data => {
   console.log(JSON.stringify(data, null, 4))
 })
+
+const getComments = async() => {
+  const comments =  await prisma.query.comments(null, '{ id text author { name } post { title } }')
+  console.log(JSON.stringify(comments, null, 4))
+  return comments
+}
+
+getComments().then(data => console.log('End comments function'))
