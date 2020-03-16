f50232b425233ab416919f88df1c4f9841acb66d
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-16 17:41:43 +0100

    feat(src/prisma.js) Challenge get posts
    Getting posts after modify one (published and body)

--- src/prisma.js
+++ src/prisma.js
@@ -5,14 +5,44 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{ id name email posts { id title } }').then(data => {
-  console.log(JSON.stringify(data, null, 4))
-})
+// prisma.query.users(null, '{ id name email posts { id title } }').then(data => {
+//   console.log(JSON.stringify(data, null, 4))
+// })
 
-const getComments = async() => {
-  const comments =  await prisma.query.comments(null, '{ id text author { name } post { title } }')
-  console.log(JSON.stringify(comments, null, 4))
-  return comments
-}
+// const getComments = async() =>
+//   await prisma.query.comments(null, '{ id text author { name } post { title } }')
 
-getComments().then(data => console.log('End comments function'))
+// getComments().then(data => JSON.stringify(data, null, 4)).then(console.log)
+
+// prisma.mutation.createPost({
+//   data: {
+//     title: '101 Graphql',
+//     body: '',
+//     published: false,
+//     author: {
+//       connect: {
+//         id: 'ck7ul9gwd00cx0879qnf872yo'
+//       }
+//     }
+//   }
+// }, '{ id title body published }')
+//   .then(data => JSON.stringify(data, null, 4))
+//   .then(console.log)
+//   .then(_ => prisma.query.users(null, '{ id name email posts { id title } }'))
+//   .then(data => JSON.stringify(data, null, 4))
+//   .then(console.log)
+
+const updatePost = _ => prisma.mutation.updatePost({
+  where: {
+    id: 'ck7uoqcf800ig0879tkf666a0'
+  },
+  data: {
+    published: true,
+    body: 'Content for the 101 course of GraphQL'
+  }
+}, '{id title body published}')
+
+updatePost()
+  .then(_ => prisma.query.posts(null, '{ id title body published }'))
+  .then(d => JSON.stringify(d, null, 2))
+  .then(console.log)
