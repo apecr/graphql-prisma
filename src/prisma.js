d124834d69881763370d5821189b83b358226dda
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-17 14:06:54 +0100

    feat(src/prisma.js) improve function createPost
    Improve createPost using prisma.exists

--- src/prisma.js
+++ src/prisma.js
@@ -5,49 +5,13 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 })
 
-// prisma.query.users(null, '{ id name email posts { id title } }').then(data => {
-//   console.log(JSON.stringify(data, null, 4))
-// })
-
-// const getComments = async() =>
-//   await prisma.query.comments(null, '{ id text author { name } post { title } }')
-
-// getComments().then(data => JSON.stringify(data, null, 4)).then(console.log)
-
-// prisma.mutation.createPost({
-//   data: {
-//     title: '101 Graphql',
-//     body: '',
-//     published: false,
-//     author: {
-//       connect: {
-//         id: 'ck7ul9gwd00cx0879qnf872yo'
-//       }
-//     }
-//   }
-// }, '{ id title body published }')
-//   .then(data => JSON.stringify(data, null, 4))
-//   .then(console.log)
-//   .then(_ => prisma.query.users(null, '{ id name email posts { id title } }'))
-//   .then(data => JSON.stringify(data, null, 4))
-//   .then(console.log)
-
-// const updatePost = _ => prisma.mutation.updatePost({
-//   where: {
-//     id: 'ck7uoqcf800ig0879tkf666a0'
-//   },
-//   data: {
-//     published: true,
-//     body: 'Content for the 101 course of GraphQL'
-//   }
-// }, '{id title body published}')
-
-// updatePost()
-//   .then(_ => prisma.query.posts(null, '{ id title body published }'))
-//   .then(d => JSON.stringify(d, null, 2))
-//   .then(console.log)
-
 const createPostForUserAndRetrieveUserInfo = async({ authorId, data }) => {
+  const userExist = await prisma.exists.User({ id: authorId })
+
+  if (!userExist) {
+    throw new Error('User not found')
+  }
+
   const post = await prisma.mutation.createPost({
     data: {
       ...data,
@@ -57,16 +21,11 @@ const createPostForUserAndRetrieveUserInfo = async({ authorId, data }) => {
         }
       }
     }
-  }, '{ id }')
-  const user = await prisma.query.user({
-    where: {
-      id: authorId
-    }
-  }, '{ id name email posts { id title published } }')
-  return user
+  }, '{ author { id name email posts { id title published } } }')
+  return post.author
 }
 
-const updatePostForUserAndRetrieveUserInfo = async({ postId, data}) => {
+const updatePostForUserAndRetrieveUserInfo = async({ postId, data }) => {
   const user = await prisma.mutation.updatePost({
     where: {
       id: postId
@@ -80,22 +39,24 @@ const updatePostForUserAndRetrieveUserInfo = async({ postId, data}) => {
   }, '{ id name email posts { id title published } }')
 }
 
-updatePostForUserAndRetrieveUserInfo({
-  postId: 'ck7vumlg800ja087919y946cr',
+createPostForUserAndRetrieveUserInfo({
+  authorId: 'ck7ul9gwd00cx0879qnf872yo',
   data: {
-    title: 'Great books to read II',
-    body: 'War of art, Crimen y Castigo',
+    title: '',
+    body: '',
     published: false
   }
 }).then(user => JSON.stringify(user, null, 2))
   .then(console.log)
+  .catch(console.error)
 
 // createPostForUserAndRetrieveUserInfo({
-//   authorId: 'ck7ugkybg004o08792rjcs7pf',
+//   authorId: '122345',
 //   data: {
-//     title: 'Great books to read',
-//     body: 'War of art',
-//     published: true,
+//     title: '',
+//     body: '',
+//     published: false
 //   }
 // }).then(user => JSON.stringify(user, null, 2))
 //   .then(console.log)
+//   .catch(console.error)
