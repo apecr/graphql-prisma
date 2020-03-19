5403a4a32f985f6c4d0fd4143ec26438c671193a
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-19 09:10:27 +0100

    feat() Inculde password field to users and lock the prisma API

--- src/prisma.js
+++ src/prisma.js
@@ -3,7 +3,8 @@ import colors from 'colors'
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466'
+  endpoint: 'http://localhost:4466',
+  secret: 'thisismysupersecrettext'
 })
 
 export { prisma as default }
