fe959a04347bd18241b94d5965c7e3326d09c28e
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-23 17:01:28 +0100

    chore() Extract secret
    Extract PRISMA_SECRET to a env variable

--- src/prisma.js
+++ src/prisma.js
@@ -5,7 +5,7 @@ import colors from 'colors'
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'thisismysupersecrettext',
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 })
 
