de9c55a34d7d8f57ea02352035af0a4f47f9a4d4
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-24 11:24:13 +0100

    test() Include some tests
    Include some tests and the jest library to configure them

--- test/user.test.js
+++ test/user.test.js
@@ -1,7 +1,25 @@
-test('This is my first test case', () => {
-  const a = 12
+/* global test, expect */
+
+import {getFirstName, isValidPassword} from './../src/utils/user.js'
+
+test('Should return first name when gives a full name', () => {
+  const firstName = getFirstName('Alberto Eyo')
+  expect(firstName).toBe('Alberto')
 })
 
-test('This is my second test case', () => {
-  const a = 12
+test('Should return first name when gives only the name', () => {
+  const name = getFirstName('Sandra')
+  expect(name).toBe('Sandra')
+})
+
+test('Should reject password shorter than 8 characters', () => {
+  expect(isValidPassword('1234567')).toBe(false)
+})
+
+test('Should reject password that contains the word password', () => {
+  expect(isValidPassword('PassWord123456')).toBe(false)
+})
+
+test('Should correctly validate a correct password', () => {
+  expect(isValidPassword('red12345')).toBe(true)
 })
