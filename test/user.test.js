e58f4f9ca00d3c8bbbb9c690620ef959362050c0
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2020-03-26 18:13:38 +0100

    test(test/user.test.js) Include createUser error test
    Include test case where the user can not be created because a weak password

--- test/user.test.js
+++ test/user.test.js
@@ -60,11 +60,9 @@ test('Should create a new user', async() => {
           }
       }
   `
-
   const response = await client.mutate({
     mutation: createUser
   })
-
   const existUser = await prisma.exists.User({
     id: response.data.createUser.user.id
   })
@@ -81,7 +79,6 @@ test('Should expose public author profiles', async() => {
       }
     }
   `
-
   const response = await client.query({query: getUsers})
   expect(response.data.users.length).toBe(1)
   expect(response.data.users[0].email).toBe(null)
@@ -99,8 +96,40 @@ test('Should return all the public posts', async() => {
       }
     }
   `
-
   const postsResponse = await client.query({ query: getPosts })
   expect(postsResponse.data.posts.length).toBe(1)
   expect(postsResponse.data.posts[0].published).toBe(true)
+})
+
+test('Should not login with bad credentials', async() => {
+  const login = gql`
+    mutation{
+      login(data: {
+        email: "jes@example.com",
+        password: "Red12345678"
+      }){
+        token
+      }
+    }
+  `
+
+  await expect(
+    client.mutate({mutation: login})
+  ).rejects.toThrow()
+})
+
+test('Should not create a user with short password', async() => {
+  const createUser = gql`
+    mutation{
+      createUser(data: {
+        email: "test@test.com",
+        password: "test",
+        name: "Test"
+      }){
+        token
+      }
+    }
+  `
+
+  await expect(client.mutate({mutation: createUser})).rejects.toThrow()
 })
